fc7a08c552755979cdedb27f26d39a7cec7967b3
dipanjan <75236099+dipanjan18-lab@users.noreply.github.com>
2024-01-11 11:23:26 +0530

    Create connectregister.php

---
+++ connectregister.php
@@ -0,0 +1,9 @@
+<?php
+    $host = "localhost"; // Host name or IP address
+    $username = "id19182739_allrounderdip"; // Mysql username
+    $password = "r)Lb-0#Hygc+%k*)s"; // Mysql password
+    $db_name = "id19182739_smartlifestyle"; // Database name
+
+    // Connect to server and select database.
+    $con = mysqli_connect($host, $username, $password, $db_name);
+?>
